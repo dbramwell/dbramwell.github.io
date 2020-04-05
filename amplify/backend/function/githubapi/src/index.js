13141508b200ff0a419fb3847bd5e2cfcc806acb
David <david.bramwell@endemolshine.com>
2020-04-05 11:04:20 +0100

    Remove buit files

--- amplify/backend/function/githubapi/src/index.js
+++
@@ -1,7 +0,0 @@
-const awsServerlessExpress = require('aws-serverless-express');
-const serverConf = require('./app');
-const server = awsServerlessExpress.createServer(serverConf);
-exports.handler = (event, context) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    awsServerlessExpress.proxy(server, event, context);
-};
--- amplify/backend/function/stackoverflowapi/src/index.js
+++
@@ -1,7 +0,0 @@
-const awsServerlessExpress = require('aws-serverless-express');
-const api = require('./app');
-const server = awsServerlessExpress.createServer(api);
-exports.handler = (event, context) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    awsServerlessExpress.proxy(server, event, context);
-};
