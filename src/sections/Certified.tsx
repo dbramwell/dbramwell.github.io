98c523ebaf1db4e0ad311bb58b2f7a8714fa3f75
David Bramwell <13053834+dbramwell@users.noreply.github.com>
2020-12-21 14:52:03 +0000

    Update Certified.tsx

--- src/sections/Certified.tsx
+++ src/sections/Certified.tsx
@@ -5,7 +5,7 @@ const Certified: React.FC = () => {
   return (
     <section style={{textAlign: "center"}}>
       <a href="https://www.youracclaim.com/badges/bb4735ce-1d2d-4a57-a58c-8993e4b81f9a/public_url">
-        <img src="https://images.youracclaim.com/size/680x680/images/2a15d440-edbe-44a2-890f-0a0caf7e1442/AWS-Developer-Associate.png" alt="AWS Certified Developer – Associate" width="150"/>
+        <img src="https://images.youracclaim.com/size/340x340/images/598f6ac6-2dbd-4394-8ae4-943b2f4c43ea/AWS-Developer-Associate-2020.png" alt="AWS Certified Developer – Associate" width="150"/>
       </a>
     </section>
   );
