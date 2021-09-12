fdc3e1f1f895e3e23283785461024b4c24308476
David Bramwell <david.j.bramwell@googlemail.com>
2021-09-12 01:05:05 +0100

    Get api working

--- api/models.py
+++ api/models.py
@@ -1,12 +1,25 @@
+import os
+
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute
+from pydantic import BaseModel, constr
+from utils import table_names
+
+if os.getenv("LOCALSTACK_HOSTNAME", False):
+    DYNAMODB_ENDPOINT = "http://" + os.getenv("LOCALSTACK_HOSTNAME") + ":4566"
 
 class User(Model):
-    """
-    A DynamoDB User
-    """
     class Meta:
-        table_name = "dynamodb-user"
-    email = UnicodeAttribute(null=True)
-    first_name = UnicodeAttribute(range_key=True)
-    last_name = UnicodeAttribute(hash_key=True)
+        host = DYNAMODB_ENDPOINT
+        table_name = table_names["User"]
+    email = UnicodeAttribute(hash_key=True)
+    first_name = UnicodeAttribute()
+    last_name = UnicodeAttribute(range_key=True)
+
+class UserModel(BaseModel):
+    email: str
+    first_name: str
+    last_name: str
+
+    class Config:
+        orm_mode = True
