2a83e661b608c90800b9f8d81396475985488cc6
David Bramwell <david.j.bramwell@googlemail.com>
2021-11-08 09:09:32 +0000

    Use docker to deploy

---
+++ infra/deploy-live.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cdk bootstrap
+cdk deploy --require-approval never DevelopmentStack
+aws cloudformation describe-stacks --stack-name DevelopmentStack --query "Stacks[0].Outputs" > /output/outputs.json
+GITHUB_ARN=$(jq -cr ".[] | select(.OutputKey | contains(\"githublambda\")) | .OutputValue" /output/outputs.json)
+SO_ARN=$(jq -cr ".[] | select(.OutputKey | contains(\"stackoverflowlambda\")) | .OutputValue" /output/outputs.json)
+echo $GITHUB_ARN
+echo $SO_ARN
+aws lambda invoke --function-name $GITHUB_ARN /tmp/out
+aws lambda invoke --function-name $SO_ARN /tmp/out
