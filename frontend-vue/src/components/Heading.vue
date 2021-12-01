e2085188cb391ea9361068b87d5fada70fbe7cb4
David Bramwell <david.j.bramwell@googlemail.com>
2021-12-01 18:26:46 +0000

    Fix layout

--- frontend-vue/src/components/Heading.vue
+++ frontend-vue/src/components/Heading.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div id="header" class="columns is-flex is-flex-wrap-wrap">
+    <div id="header" class="columns is-column-mobile is-flex is-flex-wrap-wrap">
       <div class="column is-3">
-        <figure class="image is-128x128">
+        <figure class="image">
           <img :src="avatarUrl" class="is-rounded">
         </figure>
       </div>
@@ -15,28 +15,29 @@
         <h6 class="mb-4 is-size-4 is-size-5-mobile">
           <a href="./david_bramwell_cv.pdf">Download CV</a>
         </h6>
+        <div class="is-size-2">
+          <a href="https://stackoverflow.com/users/1147618/dbramwell">
+            <i style="color: hsl(27,90%,55%);" class="mr-3 fab fa-stack-overflow"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/david-bramwell-423b2136/">
+            <i style="color: #0a66c2;" class="mr-3 fab fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/dbramwell">
+            <i style="color: #000000;" class="fab fa-github-square"></i>
+          </a>
+        </div>
+        <div class="is-flex is-justify-content-flex-start">
+          <a href="https://www.credly.com/badges/bb4735ce-1d2d-4a57-a58c-8993e4b81f9a/public_url">
+            <figure class="image is-128x128">
+              <img src="../assets/AWS-Developer-Associate-2020.png">
+            </figure>
+          </a>
+        </div>
       </div>
     </div>
+    <h2 class="is-size-3 is-size-4-mobile">{{ summary }}</h2>
     <div>
-      <h2 class="is-size-3 is-size-4-mobile">{{ summary }}</h2>
-      <div class="is-size-2">
-        <a href="https://stackoverflow.com/users/1147618/dbramwell">
-          <i style="color: hsl(27,90%,55%);" class="mr-3 fab fa-stack-overflow"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/david-bramwell-423b2136/">
-          <i style="color: #0a66c2;" class="mr-3 fab fa-linkedin"></i>
-        </a>
-        <a href="https://github.com/dbramwell">
-          <i style="color: #000000;" class="fab fa-github-square"></i>
-        </a>
-      </div>
-      <div class="is-flex is-justify-content-flex-start">
-        <a href="https://www.credly.com/badges/bb4735ce-1d2d-4a57-a58c-8993e4b81f9a/public_url">
-          <figure class="image is-128x128">
-            <img src="../assets/AWS-Developer-Associate-2020.png">
-          </figure>
-        </a>
-      </div>
+      
     </div>
   </div>
 </template>
