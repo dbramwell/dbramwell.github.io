6bdcffc0ad72229b8e87b89e6fcd5bcacea3c530
David Bramwell <david.j.bramwell@googlemail.com>
2021-12-01 18:02:52 +0000

    Fix up local env

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+awslocal ssm put-parameter --name GITHUB_TOKEN --value $GITHUB_TOKEN --type SecureString
+awslocal ssm put-parameter --name STACKOVERFLOW_USER --value $STACKOVERFLOW_USER --type SecureString
 cdklocal bootstrap
 cdklocal deploy --require-approval never DevelopmentStack
 awslocal cloudformation describe-stacks --stack-name DevelopmentStack --query "Stacks[0].Outputs" > /output/outputs.json
